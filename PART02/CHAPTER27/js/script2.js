eedb3d154872f65e4976d2e0af7d323d208fb509
Shota Yamahira <seak0503@gmail.com>
2016-11-14 07:09:49 +0900

    CHAPTER27 完了

--- PART02/CHAPTER27/js/script2.js
+++ PART02/CHAPTER27/js/script2.js
@@ -24,7 +24,61 @@ $(function () {
         '<a href="javascript:void(0);" class="btnNext"></a>' +
         '<div class="pagiNation"></div>'
       ).find('ul').wrapAll('<div class="slideWrap" />');
-    }
 
-  })
-})
+      var pagination = self.find('.pagiNation');
+
+      $.each(new Array(wrapSplit), function (i) {
+        pagination.append('<a href="javascript:void(0);" class="pn' + (i + 1) + '"></a>');
+      });
+
+      var pnPoint = pagination.find('a'),
+          pnFirst = pagination.find('a:first'),
+          pnLast = pagination.find('a:last'),
+          pnCount = pagination.find('a').length;
+
+      pnFirst.addClass('pnActive');
+
+      pnPoint.click(function () {
+        if (!(selfUl.is(':animated'))) {
+          var showCont = pnPoint.index(this);
+
+          selfUl.animate({left: '-' + ((listWidth * moveCount) * showCont)}, slideSpeed);
+
+          pnPoint.removeClass('pnActive');
+          $(this).addClass('pnActive');
+        }
+      });
+
+      var btnNext = self.find('.btnNext'),
+          btnPrev = self.find('.btnPrev');
+
+      btnNext.click(function () {
+        if (!(selfUl.is(':animated'))) {
+          var setActive = pagination.find('.pnActive'),
+              pnIndex = pnPoint.index(setActive),
+              pnNum = pnIndex + 1;
+
+          if (pnCount == pnNum) {
+            pnFirst.click();
+          } else {
+            setActive.next('a').click();
+          }
+        }
+      });
+
+      btnPrev.click(function () {
+        if (!(selfUl.is(':animated'))) {
+          var setActive = pagination.find('.pnActive'),
+              pnIndex = pnPoint.index(setActive),
+              pnNum = pnIndex + 1;
+
+          if (1 == pnNum) {
+            pnLast.click();
+          } else {
+            setActive.prev('a').click();
+          }
+        }
+      });
+    }
+  });
+});
