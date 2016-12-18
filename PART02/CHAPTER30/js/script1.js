740595074ed0cddee5f4e046ee746d68334f5dc0
Shota Yamahira <seak0503@gmail.com>
2016-12-19 07:38:06 +0900

    ナビゲーションボタン実装中

--- PART02/CHAPTER30/js/script1.js
+++ PART02/CHAPTER30/js/script1.js
@@ -52,6 +52,48 @@ $(function () {
 
         findWrap.css({left: -(baseWrapWidth), width: allLWrapWidth, height: imgHeight});
         findWrap.find('ul').css({width: baseWrapWidth, height: imgHeight});
+
+        // ページネーション設定
+        var pagination = $('<div class="pagiNation"></div>');
+        self.append(pagination);
+
+        findLi.each(function (i) {
+          pagination.append('<a href="javascript:void(0);" class="pn' + (i + 1) + '"></a>');
+        });
+
+        var pnPoint = pagination.find('a'),
+            pnFirst = pagination.find('a:first'),
+            pnLast = pagination.find('a:last'),
+            pnCount = pagination.find('a').length;
+
+        pnFirst.addClass('pnActive');
+
+        pnPoint.click(function () {
+          //timerStop();
+          var showCont = pnPoint.index(this),
+              moveLeft = (imgWidth * showCont) + baseWrapWidth;
+          findWrap.stop().animate({left: -(moveLeft)}, slideSpeed, slideEasing);
+          pnPoint.removeClass('pnActive');
+          $(this).addClass('pnActive');
+          activePos();
+          //timerStart();
+        });
+
+        function activePos() {
+          var posActive = pagination.find('.pnActive'),
+              posIndex = pnPoint.index(posActive);
+          findLi.removeClass('mainActive').eq(posIndex).addClass('mainActive');
+        }
+
+        // ナビゲーションボタン
+        self.append(
+          '<a href="javascript:void(0);" class="btnPrev"></a>' +
+          '<a href="javascript:void(0);" class="btnNext"></a>'
+        )
+        var btnNext = self.find('.btnNext'),
+            btnPrev = self.find('.btnPrev'),
+            posResetNext = -(baseWrapWidth) * 2,
+            posResetPrev = -(baseWrapWidth) + (imgWidth);
       }
     });
   });
